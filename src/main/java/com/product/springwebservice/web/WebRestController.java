897c3fa195991556099fc7d19fc2dc25680b861e
jinhegithub <smisso71479@outlook.com>
2018-12-07 02:41:00 +0800

    create new project

---
+++ src/main/java/com/product/springwebservice/web/WebRestController.java
@@ -0,0 +1,12 @@
+package com.product.springwebservice.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WebRestController {
+    @GetMapping("/hello")
+    public String hello(){
+        return "Hello World";
+    }
+}
